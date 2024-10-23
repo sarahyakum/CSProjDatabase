f9672e5162dd07c4786d1c60dd4abadd2a3e315a
daryaanbar <anbardarya22@gmail.com>
2024-10-23 14:20:29 -0500

    added/removed views

--- SQL/create_views.sql
+++ SQL/create_views.sql
@@ -23,32 +23,13 @@ FROM MemberOf M
 JOIN Student S ON M.StuNetID = S.StuNetID;
 
 
--- View for all sections of all professors
-CREATE VIEW professor_sections AS
-SELECT P.ProfName, P.ProfNetID, Sec.SecName, Sec.SecCode
-FROM Professor P
-JOIN Teaches T ON P.ProfNetID = T.ProfNetID
-JOIN Section Sec ON T.SecCode = Sec.SecCode;
-
-
--- View for all students in all teams of all sections of all professors
-CREATE VIEW professor_students AS
-SELECT P.ProfName, P.ProfNetID, Sc.SecName, Sc.SecCode, T.TeamNum, S.StuNetID, S.StuName
-FROM Professor P
-JOIN Teaches Te ON P.ProfNetID = Te.ProfNetID
-JOIN Section Sc ON Te.SecCode = Sc.SecCode
-JOIN Team T ON Sc.SecCode = T.SecCode
-JOIN MemberOf M ON T.TeamNum = M.TeamNum
-JOIN Student S ON M.StuNetID = S.StuNetID;
-
-
--- View for all criteria categories for all sections of all professors
-CREATE VIEW professor_peer_review_criteria AS
-SELECT P.ProfNetID, C.CriteriaName, C.CriteriaDescription, Sc.SecCode
-FROM Professor P
-JOIN Teaches T ON T.ProfNetID = P.ProfNetID
-JOIN Section Sc ON Sc.SecCode = T.SecCode
-JOIN Criteria C ON C.SecCode = Sc.SecCode;
+-- View for all scores that all students received in all sections (for all criteria)
+CREATE VIEW student_scores_received AS
+SELECT S.StuNetID, R.SecCode, C.CriteriaName, C.ReviewType, Sc.Score
+FROM Student S 
+JOIN Reviewed R ON R.StuNetID = S.StuNetID
+JOIN Scored Sc ON Sc.ReviewID = R.ReviewID
+JOIN Criteria C ON C.CriteriaID = Sc.CriteriaID;
 
 
 -- View for all scores that all students received in all professors' sections (for all criteria)
