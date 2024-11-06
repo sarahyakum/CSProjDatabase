ed6d5c811ecbcb1b7eb4099d497bdb7df0a818af
daryaanbar <anbardarya22@gmail.com>
2024-11-06 12:44:44 -0600

    removed unused views

--- SQL/create_views.sql
+++ SQL/create_views.sql
@@ -9,6 +9,7 @@
 USE seniordesignproject;
 
 -- View for all timeslots for all students in the system
+-- Used in these procedures: student_timeslot_by_date, student_timeslot_by_week, student_timeslot_by_month
 CREATE VIEW student_timeslots AS
 SELECT S.StuNetID, S.StuName, T.TSDate, T.TSDescription, T.TSDuration
 FROM Student S
@@ -16,6 +17,7 @@ JOIN Timeslot T ON T.StuNetID = S.StuNetID;
 
 
 -- View for all criteria categories for all students in the system
+-- Used in these procedures: student_get_peer_review_criteria
 CREATE VIEW student_peer_review_criteria AS
 SELECT S.StuNetID, C.CriteriaName, C.CriteriaDescription, Sc.SecCode
 FROM Student S
@@ -25,41 +27,15 @@ JOIN Criteria C ON C.SecCode = Sc.SecCode;
 
 
 -- View for team number and section code for all students
+-- Used in these procedures: student_get_team_members
 CREATE VIEW student_team_and_section AS
 SELECT M.TeamNum, M.SecCode, S.StuNetID, S.StuName
 FROM MemberOf M
 JOIN Student S ON M.StuNetID = S.StuNetID;
 
 
--- View for all sections of all professors
-CREATE VIEW professor_sections AS
-SELECT P.ProfName, P.ProfNetID, Sec.SecName, Sec.SecCode
-FROM Professor P
-JOIN Teaches T ON P.ProfNetID = T.ProfNetID
-JOIN Section Sec ON T.SecCode = Sec.SecCode;
-
-
--- View for all students in all teams of all sections of all professors
-CREATE VIEW professor_students AS
-SELECT P.ProfName, P.ProfNetID, Sc.SecName, Sc.SecCode, T.TeamNum, S.StuNetID, S.StuName
-FROM Professor P
-JOIN Teaches Te ON P.ProfNetID = Te.ProfNetID
-JOIN Section Sc ON Te.SecCode = Sc.SecCode
-JOIN Team T ON Sc.SecCode = T.SecCode
-JOIN MemberOf M ON T.TeamNum = M.TeamNum
-JOIN Student S ON M.StuNetID = S.StuNetID;
-
-
--- View for all criteria categories for all sections of all professors
-CREATE VIEW professor_peer_review_criteria AS
-SELECT P.ProfNetID, C.CriteriaName, C.CriteriaDescription, Sc.SecCode
-FROM Professor P
-JOIN Teaches T ON T.ProfNetID = P.ProfNetID
-JOIN Section Sc ON Sc.SecCode = T.SecCode
-JOIN Criteria C ON C.SecCode = Sc.SecCode;
-
-
 -- View for all scores that all students received in all professors' sections (for all criteria)
+-- Used in these procedures: professor_view_averages
 CREATE VIEW professor_student_scores AS
 SELECT P.ProfNetID, Sec.SecCode, M.TeamNum, S.StuNetID AS RevieweeNetID, 
 PR.ReviewerID AS ReviewerNetID, PR.ReviewType, C.CriteriaName, Sc.score
